d393a635cfeda72ee80978ba99f82aa8dca4b4a1
Jim Devine <jiim.devine@gmail.com>
2021-01-11 15:56:32 -0500

    adds function skeleton, example and test cases

--- 01-sliding-window/04-no-repeat-substring.py
+++ 01-sliding-window/04-no-repeat-substring.py
@@ -22,3 +22,32 @@ Example 3:
     Explanation: Longest substrings without any repeating characters are "abc" & "cde".
 """
 
+
+
+def no_repeat_substring(str):
+    pass
+
+
+
+
+if __name__ == "__main__":
+
+    example_1 ="aabccbb"
+    expected_1 = 3
+
+    example_2 = "abbbb"
+    expected_2 = 2
+
+    example_3 = "abccde"
+    expected_3 = 3
+
+    print(f"output: {no_repeat_substring(example_1)}, exp: {expected_1}")
+    print(f"output: {no_repeat_substring(example_2)}, exp: {expected_2}")
+    print(f"output: {no_repeat_substring(example_3)}, exp: {expected_3}")
+
+    assert no_repeat_substring(example_1) == expected_1
+    assert no_repeat_substring(example_2) == expected_2
+    assert no_repeat_substring(example_3) == expected_3
+
+    print("-"*20)
+    print("All Tests Passed!")
