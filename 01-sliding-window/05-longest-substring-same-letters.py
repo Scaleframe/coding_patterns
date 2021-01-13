75d123f441f43535780be9a1766e5f2ed1fcbc16
Jim Devine <jiim.devine@gmail.com>
2021-01-12 22:16:28 -0500

    adds example and test cases

--- 01-sliding-window/05-longest-substring-same-letters.py
+++ 01-sliding-window/05-longest-substring-same-letters.py
@@ -23,3 +23,36 @@ Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating subst
 """
 
 
+def longest_substring(str, k):
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+
+    example_1 = "aabccbb"
+    k_1 = 2
+    expected_1 = 5
+
+
+    # example_2 = "abbcb"
+    # k_2 = 1
+    # expected_2 = 4
+
+
+    # example_3 = "abccde"
+    # k_3 = 1
+    # expected_3 = 3
+
+    print(f"output: {longest_substring(example_1, 2)}, exp: {expected_1}")
+    # print(f"output: {longest_substring(example_2, 1)}, exp: {expected_2}")
+    # print(f"output: {longest_substring(example_3, 1)}, exp: {expected_3}")
+
+    assert longest_substring(example_1, 2) == expected_1
+    # assert longest_substring(example_2, 1) == expected_2
+    # assert longest_substring(example_3, 1) == expected_3
+
+    print("-"*20)
+    print("All Tests Passed!")
