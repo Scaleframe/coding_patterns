f53fc03327e2916d48c8fdcc6db20af16b06f24d
Jim Devine <jiim.devine@gmail.com>
2021-01-12 23:02:38 -0500

    adds function that applies sliding window with k spaces left. adds example notes.

--- 01-sliding-window/05-longest-substring-same-letters.py
+++ 01-sliding-window/05-longest-substring-same-letters.py
@@ -21,12 +21,60 @@ Input: String="abccde", k=1
 Output: 3
 Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
 """
+from collections import defaultdict
 
 
 def longest_substring(str, k):
-    pass
+    window_start = 0
+    max_length = 0
+    max_repeat_letter_count = 0
+    frequency_map = defaultdict(int)
+
+    # expand window forward, adding values frequency table
+    for window_end in range(len(str)):
+        right_char = str[window_end]
+        frequency_map[right_char] += 1
+        max_repeat_letter_count = max(
+            max_repeat_letter_count, frequency_map[right_char])
+        
+    
+        # check if window has > k "extra slots" open. 
+                            # if not, shrink window.
+                            # (notes below) 
+        if (window_end - window_start + 1 - max_repeat_letter_count) > k:
+            left_char =  str[window_start]
+            frequency_map[left_char] -= 1
+            window_start += 1 
+
+        max_length = max(max_length, window_end - window_start + 1)
+    return max_length
 
 
+"""
+Notes:
+
+    Take the example abbcbbc, k=2
+
+    We want to move through the string adding to frequency_count
+
+        a, b, b, c, b, b, c
+        a:1, b:1, b:2, c:1, b:3, b:4, c: 2
+
+    To get the max length, we want to return the legnth of this window
+
+    However, there is condition we want to shrink the window, 
+
+        whenever there is more room in the window than there is 
+        k places to replace characters. Take the string above.
+
+    [a, b, b, c, b, b,] c <--- you can safely grow your window until here.
+        why? 
+            window size is 6 
+            max_repeating currently is 4
+            window size - max repeating gives you "spaces left"
+            "spaces left" is what you can replace
+            if you make sure "spaces left" = k, you are in business. 
+"""
 
 
 
@@ -37,22 +85,22 @@ if __name__ == "__main__":
     expected_1 = 5
 
 
-    # example_2 = "abbcb"
-    # k_2 = 1
-    # expected_2 = 4
+    example_2 = "abbcb"
+    k_2 = 1
+    expected_2 = 4
 
 
-    # example_3 = "abccde"
-    # k_3 = 1
-    # expected_3 = 3
+    example_3 = "abccde"
+    k_3 = 1
+    expected_3 = 3
 
     print(f"output: {longest_substring(example_1, 2)}, exp: {expected_1}")
-    # print(f"output: {longest_substring(example_2, 1)}, exp: {expected_2}")
-    # print(f"output: {longest_substring(example_3, 1)}, exp: {expected_3}")
+    print(f"output: {longest_substring(example_2, 1)}, exp: {expected_2}")
+    print(f"output: {longest_substring(example_3, 1)}, exp: {expected_3}")
 
     assert longest_substring(example_1, 2) == expected_1
-    # assert longest_substring(example_2, 1) == expected_2
-    # assert longest_substring(example_3, 1) == expected_3
+    assert longest_substring(example_2, 1) == expected_2
+    assert longest_substring(example_3, 1) == expected_3
 
     print("-"*20)
     print("All Tests Passed!")
