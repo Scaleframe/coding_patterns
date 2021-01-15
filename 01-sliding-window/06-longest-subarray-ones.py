57ad582efeb424ca105e73608e1ec32aaef5c787
Jim Devine <jiim.devine@gmail.com>
2021-01-15 16:33:26 -0500

    adds function scaffold, examples and test cases

--- 01-sliding-window/06-longest-subarray-ones.py
+++ 01-sliding-window/06-longest-subarray-ones.py
@@ -18,3 +18,32 @@ Output: 9
 Explanation: Replace the '0' at index 6, 9, and 10 to have the longest contiguous subarray
  of 1s having length 9.
 """
+
+def length_longest_subarray(arr, k):
+    pass
+
+
+
+
+if __name__ == "__main__":
+
+    example_1 = [0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1]
+    k_1 = 2
+    expected_1 = 6
+
+
+    example_2 = [0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1]
+    k_2 = 1
+    expected_2 = 4
+
+
+    print(f"output: {length_longest_subarray(example_1, 2)}, exp: {expected_1}")
+    print(f"output: {length_longest_subarray(example_2, 1)}, exp: {expected_2}")
+
+
+    assert length_longest_subarray(example_1, 2) == expected_1
+    assert length_longest_subarray(example_2, 1) == expected_2
+
+
+    print("-"*20)
+    print("All Tests Passed!")
